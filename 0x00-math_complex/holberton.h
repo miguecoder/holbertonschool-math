e49c68676c05834dc6b82305cc9570043e02829b
miguecoder <mmarqzm@gmail.com>
2022-04-02 16:59:12 -0700

    Creation of files

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stddef.h>
+ /*Prototypes*/
+void display_complex_number(complex c);
+double modulus(complex c);
+double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg (double m, double arg, complex *c3);
+#endif
